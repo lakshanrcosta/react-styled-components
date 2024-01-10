6382fdebf51f3e240e314b3ad9882368e4fbf19b
Lakshan Costa <lakshanrcosta@gmail.com>
2024-01-10 17:43:27 +0200

    Added eslint and prettier configurations

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./common/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello from styles components</h1>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
